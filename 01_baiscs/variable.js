c4c6640e1d2cace74f3d4e05df57bbe2fdc27e5f
shivanshu0055 <159451461+shivanshu0055@users.noreply.github.com>
2024-07-01 19:24:41 +0000

    datatypes

---
+++ 01_baiscs/variable.js
@@ -0,0 +1,8 @@
+const accId=102193
+let accemail="hunny@google.com"
+var accPass="123"
+acccity="Doon"
+let X
+//Const cant be changed
+console.log(accId);
+console.table([accId,accemail,accPass,X]);
---
+++ 02_datatypes/basics.js
@@ -0,0 +1,15 @@
+"use strict";//treat all js code as newer version
+
+// alert("hello") //We are using NodeJS not browser
+
+console.log(3+3);
+console.log(44);
+
+// NULL -> standalone value -> OBJECT
+// undefined -> Value assign nahi hua hai -> type undefined
+// symbol -> uniqueness
+
+
+
+// object 
+console.log(typeof 7)
