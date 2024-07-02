ee777a5ab9929106c4f7cbe458f83ec8777cc1ba
shivanshu0055 <159451461+shivanshu0055@users.noreply.github.com>
2024-07-02 11:48:34 +0000

    Conversion

---
+++ 01_baiscs/conversion.js
@@ -0,0 +1,27 @@
+let score="90"
+// console.log(typeof score)
+
+let valueInNum=Number(score)
+// console.log(typeof valueInNum)
+
+/*NAN is Also as special type ye "90asb" 
+ko bhi convert kar dega num me*/
+// "33"->33
+//"33abc"->NaN
+//true->1
+//NULL->0
+
+let isLogeedIn=""
+
+let booleanIsLoggedIn=Boolean(isLogeedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1-> true; 0->false
+// ""->false
+//"Hunny"->true
+
+
+let something=33
+let toString=String(something)
+console.log(toString);
