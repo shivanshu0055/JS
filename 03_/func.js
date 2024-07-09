105c65cdb02daa6aff8c19f13c1f82b4cd8435af
shivanshu0055 <159451461+shivanshu0055@users.noreply.github.com>
2024-07-09 04:46:32 +0000

    func

---
+++ 03_/func.js
@@ -0,0 +1,33 @@
+function sayMyName(){
+    console.log("Poori life me to underdog tha : ");
+}
+
+sayMyName()
+
+function add(num1,num2) {
+    console.log(num1+num2);
+}
+
+const res=add(10,30)
+
+console.log("RES : ",res);
+
+function greet(name = "Sammy"){
+    return `Helllo ${name}`
+}
+
+// function calcPrice(...num1){
+//     return num1
+// }
+
+ function calcPrice(val1,val2,...num1){
+        return num1
+    }
+    
+console.log(calcPrice(100,200,300));
+
+//Objects bhi generic way me
+
+function seeusername(obj){
+    return `hello ! ${obj.username}`
+}
