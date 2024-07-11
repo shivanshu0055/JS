610ebfd4f11014db7f3aa6fdb54226a87fddac49
shivanshu0055 <159451461+shivanshu0055@users.noreply.github.com>
2024-07-11 08:08:50 +0000

    flow

---
+++ controlflow.js
@@ -0,0 +1,32 @@
+switch(month){
+    case 1:
+        console.log("first");
+        break
+    case 2:
+        console.log("first");
+        break
+    case 3:
+        console.log("first");
+        break
+    case 4:
+        console.log("first");
+        break
+    default:
+        console.log("HELLO");
+    
+}
+//String aae toh bhi kar sakte case ke age
+// 1 -> "jan"
+
+//falsy
+//fasle,0,-0,BigInt 0n,"",null,undefined,NaN
+
+//truthy
+// "0","false"," ",[],{},function(){}
+
+const empObj={}
+
+if(Object.keys(empObj).length===0){
+    console.log("EMPTY");
+}
+
