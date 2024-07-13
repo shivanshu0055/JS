4d609a2464c636e8c1d4c1032c8a11641584b600
shivanshu0055 <159451461+shivanshu0055@users.noreply.github.com>
2024-07-13 09:33:53 +0000

    two

--- iterations/two.js
+++ iterations/two.js
@@ -31,6 +31,7 @@ const myObj={
 for (const key in myObj) {
     console.log(myObj[key]);
 }
+
 //map is not iteratable using forin
 for (const key in map) {
     console.log(key);
@@ -38,6 +39,9 @@ for (const key in map) {
 
 const myArr=["js","java","py","cpp"]
 
+for (const index in myArr) {
+    console.log(index);
+}
 myArr.forEach(function (item){
     console.log(item);
 })
@@ -82,6 +86,89 @@ const myCoding = [
 ]
 
 myCoding.forEach( (item) => {
-    
     console.log(item.languageName);
 } )
+
+// for each doesnt returns anything 
+
+// const myNums=[1,2,3,4,4,5,6,7,8]
+
+// const newnums=myNums.filter( (nums) => (nums>4))
+
+// const newnums=myNums.filter( (nums) => {return nums>4})
+// console.log(newnums);
+
+// const newNums=[]
+// myNums.forEach((num) => {
+//     if(num>4){
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+  userBooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+  console.log(userBooks);
+
+
+  const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+  const newNums = myNumers.map( (num) => { return num < 10})
+  console.log(newNums);
+  
+//   const newNumss = myNumers
+//                   .map((num) => num * 10 )
+//                   .map( (num) => num + 1)
+//                   .filter( (num) => num >= 40)
+
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
